a168a89b40ce127cf5041d0fa2ee96eb1569a4f8
habibulilah2 <lhabibulilah@gmail.com>
2022-04-01 06:38:35 +0700

    finishing

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Intro from "./components/intro/Intro"
+import About from "./components/about/About"
+import ProductList from "./components/productList/ProductList"
+import Contact from "./components/contact/Contact"
+import Toggle from "./components/toggle/Toggle"
+import { useContext } from "react"
+import { ThemeContext } from "./context"
+
+const App = () => {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+  return (
+    <div
+      style={{
+        backgroundColor: darkMode ? "#222" : "white",
+        color: darkMode && "white",
+      }}
+    >
+      <Toggle />
+      <Intro />
+      <About />
+      <ProductList />
+      <Contact />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://cdn.dribbble.com/users/3967258/screenshots/15463803/media/4fddb9a2caf3b3bd634060f706a91e73.png?compress=1&resize=1200x900",
+      link: "http://lama.dev",
+    },
+    {
+      id: 2,
+      img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
+      link: "http://lama.dev",
+    },
+    {
+      id: 3,
+      img: "https://cdn.dribbble.com/users/3637068/screenshots/15467010/media/69a3279182b75365013fe285733d1122.png?compress=1&resize=1200x900",
+      link: "http://lama.dev",
+    },
+    {
+      id: 4,
+      img: "https://cdn.dribbble.com/users/2264844/screenshots/15463998/media/d85e92a332c41094f68e1f716884f7ce.jpg?compress=1&resize=1200x900",
+      link: "http://lama.dev",
+    },
+    {
+      id: 5,
+      img: "https://cdn.dribbble.com/users/387658/screenshots/15458608/media/e92ec1dd33dd1170ae3dc9c5272f2693.jpg?compress=1&resize=1200x900",
+      link: "http://lama.dev",
+    },
+    {
+      id: 6,
+      img: "https://cdn.dribbble.com/users/3307260/screenshots/15468444/media/447b4501f7a145b05e11c70199a9c417.jpg?compress=1&resize=1200x900",
+      link: "http://lama.dev",
+    },
+  ];
